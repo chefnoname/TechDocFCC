1927bfd64f131dfcd8e22a456a01f4fc1e3cae18
Chef NoName <abdijama21@gmail.com>
2021-07-14 16:42:54 +0100

    update

--- main.js
+++ main.js
@@ -127,7 +127,7 @@ clipboard.addEventListener("click", () => {
       textBubble.style.display = "block";
       setTimeout(() => {
         textBubble.style.display = "none";
-      }, 1000);
+      }, 10000);
     })
     .catch(() => {
       Alert("Failed ");
